98d00c6a0f609891d18227c3579b21e08d3f7148
Amy <llamanozz@gmail.com>
2021-10-21 15:10:40 +0200

    Fixed img positioning for single link posts

--- src/components/singlepage/SingleLinkPost.js
+++ src/components/singlepage/SingleLinkPost.js
@@ -3,13 +3,16 @@ import React from 'react'
 export default function SingleLinkPost({ clickedPost, flairStyle, flairDisplay }) {
   return (
     <>
+      <div style={{display: "flex"}}>
       <div className="singlepost-title-shortened">
         <span>{clickedPost.title}</span>
         {clickedPost.link_flair_text && <span className="singlepost-flair" style={flairStyle}>{flairDisplay.length > 0 ? flairDisplay : clickedPost.link_flair_text}</span>}
       </div>
       <div className="singlepost-link-image-container">
-        {clickedPost.thumbnail_height && <img className="singlepost-link-image" src={clickedPost.thumbnail} alt="" /> }
+        <img className="singlepost-link-image" src={clickedPost.thumbnail} alt="" />
       </div>
+      </div>
+
       <div className="post-link-container">
         {clickedPost.url_overridden_by_dest && <a className="singlepost-link" href={clickedPost.url_overridden_by_dest} target="_blank" rel="noreferrer">{clickedPost.url_overridden_by_dest.slice(12, 36)}...</a>}
       </div>

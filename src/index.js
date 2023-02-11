b92e469488e9141ae580c5bb12a239520ac84023
Amy <llamanozz@gmail.com>
2023-02-11 15:32:48 +0100

    Updating number of trending links and their encoding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-import "./css/main.css";
+import "./scss/main.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
